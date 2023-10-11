cf57378440f5b2d7f29fd2f5ee5ad576e8d35b47
ANAVGAR <anavgar@etsinf.upv.es>
2023-10-11 11:00:52 +0200

    Chat

--- lib/VentanaChat.dart
+++ lib/VentanaChat.dart
@@ -10,9 +10,52 @@ class VentanaChat extends StatelessWidget{
       appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Chat"),
       ),
-      body: const Center(
-        child: Text("Chat"),
-      ),
+      body: const ChatList(),
     );
   }
+}
+
+class ChatList extends StatelessWidget {
+  const ChatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'UserID',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Chat extends StatelessWidget {
+  const Chat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: (){},
+        child: const Center(
+          child: Column(
+            children: [
+              Text("Usuario"),
+              Text("Ultimo mensaje"),
+            ],
+          ),
+
+        )
+    );
+  }
+
 }
