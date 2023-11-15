05bab0825a36d2743d0bb6fc4f0c3f6ab610fe4b
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2023-11-15 20:30:55 +0100

    errores

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:collectify/ConexionBD.dart';
 import 'package:flutter/services.dart';
-import 'package:uni_links/uni_links.dart';
 import 'VentanaInicio.dart';
 
 //Placeholder, cambiar
 Usuario u = new Usuario();
 
 void main(){
-  runURILINKS();
   runApp(const MyApp());
   Conexion().conectar();
-}
-
-Future<void> runURILINKS() async {
-  try{
-      WidgetsFlutterBinding.ensureInitialized();
-      String? initialLink = await getInitialLink();
-      handleLink(initialLink);
-
-      uriLinkStream.listen((Uri? uri) {
-      handleLink(uri.toString());
-    });
-  } on PlatformException {
-    // Handle exception by warning the user their action did not succeed
-    // return?
-  }
-}
-void handleLink(String? link) {
-  if (link != null) {
-    print(link);
-  }
 }
 
 class MyApp extends StatelessWidget { //Punto inicial, no tocar
