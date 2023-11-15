e801fda84fe8705a3abbcd1d7068eb732aa3d153
Sergi Barberà Barberà <SBARBAR@upv.edu.es>
2023-11-15 12:17:23 +0100

    boton compartir

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:collectify/ConexionBD.dart';
-
+import 'package:uni_links/uni_links.dart';
 import 'VentanaInicio.dart';
 
 //Placeholder, cambiar
 Usuario u = new Usuario();
-void main() {
-  //WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  String? initialLink = await getInitialLink();
+  handleLink(initialLink);
+
   runApp(const MyApp());
   Conexion().conectar();
-
+  uriLinkStream.listen((Uri? uri) {
+    handleLink(uri.toString());
+  });
+}
+void handleLink(String? link) {
+  if (link != null) {
+    print(link);
+  }
 }
 
 class MyApp extends StatelessWidget { //Punto inicial, no tocar
