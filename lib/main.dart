eaf39f1127b4222ed75b1678741f65b02e288121
ByteDevEs <byteplaysyt@gmail.com>
2023-11-20 12:55:18 +0100

    Fix links

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,35 @@ import 'package:flutter/material.dart';
 import 'package:collectify/ConexionBD.dart';
 import 'VentanaInicio.dart';
 import 'package:uni_links3/uni_links.dart';
+import 'package:flutter/services.dart' show PlatformException;
 
+final StreamController<Widget> _streamController = StreamController<Widget>.broadcast();
 
 void main(){
   Conexion().conectar();
   runApp(const MyApp());
+  initUniLinks();
+}
+
+Future<void> initUniLinks() async {
+  // Platform messages may fail, so we use a try/catch PlatformException.
+  try {
+    final initialLink = await getInitialLink();
+    // Parse the link and warn the user, if it is not correct,
+    // but keep in mind it could be `null`.
+    print("Link: $initialLink");
+    handleLink(initialLink).then((value) => _streamController.add(value));
+  } on PlatformException {
+    // Handle exception by warning the user their action did not succeed
+    // return?
+  }
+  linkStream.listen((String? link) {
+    // Parse the link and warn the user, if it is not correct
+    print("Link: $link");
+    handleLink(link).then((value) => _streamController.add(value));
+  }, onError: (err) {
+    // Handle exception by warning the user their action did not succeed
+  });
 }
 
 Future<Widget> handleLink(String? link) async {
@@ -17,7 +41,8 @@ Future<Widget> handleLink(String? link) async {
     List<Producto> productos = await Conexion().getProductos();
     Usuario u = await Conexion().getUsuarioByNick('admin') as Usuario;
     for (var p in productos) {
-      if (link!.toLowerCase() == "https://collectify.es/${p.productoID}") {
+      if (link! == "https://collectify.es/${p.productoID}") {
+        print("Producto encontrado");
         return VentanaValoracion(connected: u, producto: p,);
       }
     }
@@ -27,12 +52,6 @@ Future<Widget> handleLink(String? link) async {
 
 class MyApp extends StatelessWidget { //Punto inicial, no tocar
   const MyApp({super.key});
-
-  static final StreamController<Widget> _streamController = StreamController<Widget>.broadcast();
-
-  static void refresh(Uri? link) async {
-    await handleLink(link.toString()).then((value) => _streamController.add(value));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,12 +66,16 @@ class MyApp extends StatelessWidget { //Punto inicial, no tocar
       home:  StreamBuilder<Uri?>(
         stream: uriLinkStream,
         builder: (context, snapshot) {
-          refresh(snapshot.data);
           return StreamBuilder<Widget>(
             stream: _streamController.stream,
             builder: (context, snapshot) {
               if(snapshot.hasData) return snapshot.data!;
-              return const VentanaInicio();
+              //Loading
+              return const Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
             }
           );
         }
